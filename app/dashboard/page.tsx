75888036b3996e67cac66d2aeac566f3a1a4f816
Heshan <heshan.chiu@gmail.com>
2023-11-24 12:13:39 +1300

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div>
+      <p>Dashboard Page</p>
+      <a href="/dashboard/invoices">Invoices</a>
+      <a href="/dashboard/customers">Customers</a>
+    </div>
+  );
+}
