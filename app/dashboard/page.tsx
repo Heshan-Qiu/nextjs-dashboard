0a2a1a323e9fb4d35d77ff1a2011b21c76bdab4a
Heshan <heshan.chiu@gmail.com>
2023-11-25 15:22:18 +1300

    Finished course

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <p>Dashboard Page</p>
-      <a href="/dashboard/invoices">Invoices</a>
-      <a href="/dashboard/customers">Customers</a>
-    </div>
-  );
-}
